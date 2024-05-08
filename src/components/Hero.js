531fb776d0fa3344adb057b53727c0d68f41dc95
Wei <wei@Weis-Air.lan>
2024-05-08 22:25:31 +0100

    Change loading state

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,24 +2,20 @@ import React, { useState, useEffect } from 'react';
 import './Hero.css';
 
 const Hero = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const handleReadyState = () => {
-      if (document.readyState === 'complete') {
-        setLoading(false);
-      }
-    };
+    const timeout = setTimeout(() => {
+      setLoading(true);
+    }, 500);
 
-    document.addEventListener('readystatechange', handleReadyState);
-
-    return () => document.removeEventListener('readystatechange', handleReadyState);
+    return () => clearTimeout(timeout);
   }, []);
 
   return (
     <section className="hero-banner-container">
       <div className="hero-banner">
-        <div className={`hero hero-0 ${loading ? '' : 'active'}`}>
+        <div className={`hero hero-0 ${loading ? 'active' : ''}`}>
           <div className="hero__bg"></div>
           <div className="hero__content">
             <svg className="hero__overlay" viewBox="0 0 720 405" preserveAspectRatio="xMaxYMax slice">
